5a83bd1e6b002cca3c8bef71b63e3756c51cf022
dhnlr <dhani.534@gmail.com>
2023-10-26 07:58:32 +0700

    chore: handle build error

--- src/helpers/graphql/client.ts
+++ src/helpers/graphql/client.ts
@@ -2,10 +2,10 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { persistCache, LocalStorageWrapper } from "apollo3-cache-persist";
 
 const cache = new InMemoryCache();
-await persistCache({
+persistCache({
   cache,
   storage: new LocalStorageWrapper(window.localStorage),
-});
+}).then(() => {});
 
 export const client = new ApolloClient({
   uri: import.meta.env.VITE_API_URL,
