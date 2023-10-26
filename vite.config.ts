bdd6e26daf589d38c6af4a9900e018256d46ac3c
dhnlr <dhani.534@gmail.com>
2023-10-26 08:11:07 +0700

    chore: support PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,59 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA, VitePWAOptions  } from "vite-plugin-pwa";
+
+const manifest: Partial<VitePWAOptions> = {
+	registerType: "prompt",
+	includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+	manifest: {
+		name: "Telpon - DHNLR",
+		short_name: "Telpon",
+		description: "Telpon app for goto",
+		icons: [
+			{
+				src: "/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+				purpose: "apple touch icon",
+			},
+			{
+				src: "/maskable_icon.png",
+				sizes: "225x225",
+				type: "image/png",
+				purpose: "any maskable",
+			},
+		],
+		theme_color: "#fff",
+		background_color: "#e8ebf2",
+		display: "standalone",
+		scope: "/",
+		start_url: "/",
+		orientation: "portrait",
+	},
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    jsxImportSource: "@emotion/react",
-  })],
-})
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+    }),
+    VitePWA({
+      manifest: manifest,
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+    }),
+  ],
+});
